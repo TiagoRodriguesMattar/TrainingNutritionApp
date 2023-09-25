6877f2bd25812b990ef38fdb6263c844a3d0fc6b
TiagoRodriguesMattar <20100068@notredamecampinas.net.br>
2023-09-25 10:41:56 -0300

    bottom navigation bar + login inicial + atualização intent fragmento treino

--- app/src/main/java/com/example/myapplication/TreinoFragment.java
+++ app/src/main/java/com/example/myapplication/TreinoFragment.java
@@ -1,12 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +62,22 @@ public class TreinoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_treino, container, false);
+    }
+
+    public void exerciseLibraryBtn(View view) {
+        Intent i = new Intent(getActivity(), ExerciseLibraryActivity.class);
+        startActivity(i);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button exerciseLibraryButton = view.findViewById(R.id.exerciseLibrary);
+        exerciseLibraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exerciseLibraryBtn(v);
+            }
+        });
     }
 }
