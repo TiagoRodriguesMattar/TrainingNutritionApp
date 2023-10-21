678b2d72e28e1ab41cd870bc449d9807fbed88e1
ripercario <88801928+ripercario@users.noreply.github.com>
2023-10-20 22:00:22 -0300

    Possibilitação de inserção de dados nos graficos de peso e caloria

--- app/src/main/java/com/example/myapplication/AcompanhamentoPesoActivity.java
+++ app/src/main/java/com/example/myapplication/AcompanhamentoPesoActivity.java
@@ -3,8 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.jjoe64.graphview.DefaultLabelFormatter;
@@ -17,17 +21,35 @@ import java.util.Date;
 
 public class AcompanhamentoPesoActivity extends AppCompatActivity {
 
+
+    Button insertPeso;
+    EditText Peso;
     GraphView GraficoPeso;
-    LineGraphSeries<DataPoint> series;
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[0]);
+
+    MyHelperAcoPeso myHelperAcoPeso;
+    SQLiteDatabase sqLiteDatabase;
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acompanhamento_peso);
 
+        insertPeso = (Button) findViewById(R.id.InserirPeso);
+        Peso = (EditText) findViewById(R.id.inputPeso);
         GraficoPeso = (GraphView) findViewById(R.id.GraficoPeso);
-        series = new LineGraphSeries<>(getDataPoint());
+        myHelperAcoPeso = new MyHelperAcoPeso(this);
+        sqLiteDatabase = myHelperAcoPeso.getWritableDatabase();
+
         GraficoPeso.addSeries(series);
+
+        /*GraficoPeso.getViewport().setScrollable(true);
+        GraficoPeso.getViewport().setScrollableY(true);
+
+        GraficoPeso.getViewport().setScalable(true);
+        GraficoPeso.getViewport().setScalableY(true);*/
+
 
         GraficoPeso.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
             @Override
@@ -41,20 +63,42 @@ public class AcompanhamentoPesoActivity extends AppCompatActivity {
             }
         });
 
+        exqInsert();
+
         ImageView pesoBack;
         pesoBack = findViewById(R.id.pesoBack);
         pesoBack.setOnClickListener(this :: teste);
     }
 
+    private void exqInsert(){
+        insertPeso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long xValue = new Date().getTime();
+                int yValue = Integer.parseInt(Peso.getText().toString());
+
+                myHelperAcoPeso.insertData(xValue, yValue);
+
+                series.resetData(getDataPoint());
+            }
+        });
+    }
+
+    private DataPoint[] getDataPoint() {
+        String[] columns={"xValues", "yValues"};
+        Cursor cursor = sqLiteDatabase.query("Peso", columns, null, null, null, null, null);
+        DataPoint[] dp = new DataPoint[cursor.getCount()];
+
+        for(int i = 0;i < cursor.getCount();i++){
+            cursor.moveToNext();
+            dp[i] = new DataPoint(cursor.getLong(0), cursor.getInt(1));
+        }
+
+        return dp;
+    }
+
     private void teste(View view) {
         Intent intent = new Intent(this, indexActivity.class);
         startActivity(intent);
-    }
-
-    private DataPoint[] getDataPoint(){
-        DataPoint[] dp=new DataPoint[]{
-                new DataPoint(new Date().getTime(), 65),
-        };
-        return dp;
     }
 }
--- app/src/main/java/com/example/myapplication/MonitoramentoCaloriasActivity.java
+++ app/src/main/java/com/example/myapplication/MonitoramentoCaloriasActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.jjoe64.graphview.DefaultLabelFormatter;
@@ -18,8 +22,16 @@ import java.util.Date;
 
 public class MonitoramentoCaloriasActivity extends AppCompatActivity {
 
+
+    Button insertCalorias;
+    EditText Ganho;
+    EditText Perda;
+
     GraphView GraficoCalorias;
-    LineGraphSeries<DataPoint> series;
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[0]);
+
+    MyHelperMonCal myHelperMonCal;
+    SQLiteDatabase sqLiteDatabase;
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM");
 
     @Override
@@ -27,9 +39,23 @@ public class MonitoramentoCaloriasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoramento_calorias);
 
+        insertCalorias = (Button) findViewById(R.id.InserirCal);
+        Ganho = (EditText) findViewById(R.id.inputCalGanhas);
+        Perda = (EditText) findViewById(R.id.inputCalPerdidas);
         GraficoCalorias = (GraphView) findViewById(R.id.GraficoCalorias);
-        series = new LineGraphSeries<>(getDataPoint());
+        myHelperMonCal = new MyHelperMonCal(this);
+        sqLiteDatabase = myHelperMonCal.getWritableDatabase();
+
         GraficoCalorias.addSeries(series);
+
+        /*GraficoCalorias.getViewport().setXAxisBoundsManual(true);
+        GraficoCalorias.getViewport().setYAxisBoundsManual(true);
+        GraficoCalorias.getViewport().setMinY(-30);
+        GraficoCalorias.getViewport().setMaxY(30);
+
+        GraficoCalorias.getViewport().setScrollable(true);
+        GraficoCalorias.getViewport().setScrollableY(true);*/
+
 
         GraficoCalorias.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
             @Override
@@ -43,9 +69,40 @@ public class MonitoramentoCaloriasActivity extends AppCompatActivity {
             }
         });
 
+        exqInsert();
+
         ImageView caloriasBack;
         caloriasBack = findViewById(R.id.caloriasBack);
         caloriasBack.setOnClickListener(this :: teste);
+    }
+
+    private void exqInsert(){
+        insertCalorias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long xValue = new Date().getTime();
+                int n1 = Integer.parseInt(Ganho.getText().toString());
+                int n2 = Integer.parseInt(Perda.getText().toString());
+                int yValue = n1 - n2;
+
+                myHelperMonCal.insertData(xValue, yValue);
+
+                series.resetData(getDataPoint());
+            }
+        });
+    }
+
+    private DataPoint[] getDataPoint() {
+        String[] columns={"xValues", "yValues"};
+        Cursor cursor = sqLiteDatabase.query("Calorias", columns, null, null, null, null, null);
+        DataPoint[] dp = new DataPoint[cursor.getCount()];
+
+        for(int i = 0;i < cursor.getCount();i++){
+            cursor.moveToNext();
+            dp[i] = new DataPoint(cursor.getLong(0), cursor.getInt(1));
+        }
+
+        return dp;
     }
 
     private void teste(View view) {
@@ -53,10 +110,4 @@ public class MonitoramentoCaloriasActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private DataPoint[] getDataPoint(){
-        DataPoint[] dp=new DataPoint[]{
-                new DataPoint(new Date().getTime(), 80),
-        };
-        return dp;
-    }
 }
