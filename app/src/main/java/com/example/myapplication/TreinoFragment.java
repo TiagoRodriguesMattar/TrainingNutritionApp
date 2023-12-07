7b8598c9a28dbd43397f93786a44c189d4df9c94
Gabriel Garcia <gabrielsg.2604@gmail.com>
2023-12-06 23:33:50 -0300

    final front_end
    finalização front_end das features novas e repaginação da parte de perfil, treino e nutrição

--- app/src/main/java/com/example/myapplication/TreinoFragment.java
+++ app/src/main/java/com/example/myapplication/TreinoFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -79,8 +81,12 @@ public class TreinoFragment extends Fragment {
     }
 
     public void persoTreino(View view) {
+        try{
         Intent i = new Intent(getActivity(), TreinoPersonalizadoActivity.class);
         startActivity(i);
+    }catch(Exception e){
+            Log.d("CREATION", "Erro: " + e);
+        }
     }
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
